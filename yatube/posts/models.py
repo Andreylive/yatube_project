c77f16762d3dd7daeb02306f3600447c2445b048
Andrei Pshenichnyi <andrey.pshenichniy.com>
2023-02-12 17:34:48 +0300

    Первая версия проекта

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
@@ -12,3 +11,17 @@ class Post(models.Model):
         on_delete=models.CASCADE,
         related_name='posts'
     )
+    group = models.ForeignKey(
+        'Group',
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True
+    )
+
+class Group(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField()
+    description = models.TextField()
+    
+def __str__(self):
+    return self.title
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,24 +1,18 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
-
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 
 def index(request):
-    template = 'posts/index.html'
-    title = 'Проект Yatube'
-    text = 'Это главная страница проекта Yatube'
+    posts = Post.objects.order_by('-pub_date')[:10]
     context = {
-        'title': title,
-        'text': text,
-        }
-    return render(request, template, context) 
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context) 
 
 def group_posts(request, slug):
-    template = 'posts/group_list.html'
-    title = 'Проект Yatube'
-    text = 'Здесь будет информация о группах проекта Yatube'
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     context = {
-        'title': title,
-        'text': text,
+        'group': group,
+        'posts': posts,
         }
-    return render(request, template, context)
+    return render(request, 'posts/group_list.html', context)
